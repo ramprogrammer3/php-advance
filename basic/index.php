38b39580288c5786b1b812ce42518c46a2dc007f
ram kumar <rammaniyari@gmail.com>
2024-10-07 15:25:02 +0530

    basic php setup

---
+++ basic/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php basic</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h3> <?php echo "Hello world"; ?> </h3>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
