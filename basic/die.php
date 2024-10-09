e5e566a5fe0ad51d88aa80b25d3ca8398fc5ee2c
ram kumar <rammaniyari@gmail.com>
2024-10-09 07:06:31 +0530

    die or exit

---
+++ basic/die.php
@@ -0,0 +1,11 @@
+<?php
+
+    echo "1. Some message <br>";
+    echo "2. Some message <br>";
+    echo "3. Some message <br>";
+    die("This is some error here ");
+    echo "4. Some message <br>";
+    echo "5. Some message <br>";
+
+
+?>
