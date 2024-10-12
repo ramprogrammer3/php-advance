8431b81148f8e4655382d5b76f510c2b7b0044ef
ram kumar <rammaniyari@gmail.com>
2024-10-12 14:59:39 +0530

    add category in news website

--- news/admin/add-category.php
+++ news/admin/add-category.php
@@ -1,4 +1,25 @@
-<?php include "header.php"; ?>
+<?php include "header.php"; 
+
+    if(isset($_POST['save'])){
+        
+        include 'config.php';
+        $category = mysqli_real_escape_string($conn,$_POST['cat']);
+        $sql = "SELECT category_name FROM category WHERE category_name = '{$category}'";
+        $result = mysqli_query($conn,$sql) or die("Query failed");
+
+        if(mysqli_num_rows($result) > 0){
+            echo "<p class = 'color:red;text-align:center;'>This category is already exist</p>";
+        }else{
+            $sql1 = "INSERT INTO category (category_name)
+                    VALUES ('{$category}')";
+            
+            if(mysqli_query($conn,$sql1)){
+                header("Location: {$hostname}/admin/category.php");
+            }
+        }
+    }
+
+?>
   <div id="admin-content">
       <div class="container">
           <div class="row">
@@ -7,7 +28,7 @@
               </div>
               <div class="col-md-offset-3 col-md-6">
                   <!-- Form Start -->
-                  <form action="" method="POST" autocomplete="off">
+                  <form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST" autocomplete="off">
                       <div class="form-group">
                           <label>Category Name</label>
                           <input type="text" name="cat" class="form-control" placeholder="Category Name" required>
